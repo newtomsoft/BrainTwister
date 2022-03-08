f6c93ba6ca7dd758164d08a75aaa321156f0b1b1
Thomas Vuille <thomas@newtomsoft.fr>
2022-03-08 19:04:43 +0100

    change some value tests

--- LaserBrainTwister.Tests/OptimizeTreeTests.cs
+++ LaserBrainTwister.Tests/OptimizeTreeTests.cs
@@ -1,4 +1,6 @@
-﻿namespace LaserBrainTwister.Tests;
+﻿using System;
+
+namespace LaserBrainTwister.Tests;
 
 public class OptimizeTreeTests
 {
@@ -9,8 +11,8 @@ public class OptimizeTreeTests
         var coordinates = new List<Coordinate>
         {
             Coordinate.From(0, 0), Coordinate.From(2, 0),
-            Coordinate.From(1, 2), Coordinate.From(2, 2), Coordinate.From(4, 2),
-            Coordinate.From(4, 4),
+            Coordinate.From(1, 1), Coordinate.From(2, 1), Coordinate.From(3, 1),
+            Coordinate.From(3, 2),
 
         };
         grid.SwitchCoordinatesStatus(coordinates);
@@ -20,7 +22,7 @@ public class OptimizeTreeTests
         var tree = grid.GenerateTree();
         var routes = tree.OptimizeRoutes();
         routes.Count.ShouldBe(1);
-        routes[0].ToString().ShouldBe("(0,0) (2,0) (2,2) (4,2) (4,4)");
+        routes[0].ToString().ShouldBe("(0,0) (2,0) (2,1) (3,1) (3,2)");
     }
 
     [Fact]
@@ -117,25 +119,21 @@ public class OptimizeTreeTests
         var grid = new Grid();
         var coordinates = new List<Coordinate>
         {
-            Coordinate.From(0, 0), Coordinate.From(5, 0), Coordinate.From(8, 0), Coordinate.From(10, 0), Coordinate.From(12, 0),
-            Coordinate.From(13, 0), Coordinate.From(15, 0),
+            Coordinate.From(0, 0), Coordinate.From(5, 0), Coordinate.From(8, 0), Coordinate.From(10, 0), Coordinate.From(12, 0), Coordinate.From(13, 0), Coordinate.From(15, 0),
             Coordinate.From(6, 1), Coordinate.From(7, 1), Coordinate.From(9, 1), Coordinate.From(10, 1),
-            Coordinate.From(1, 2), Coordinate.From(3, 2), Coordinate.From(4, 2), Coordinate.From(5, 2), Coordinate.From(12, 2),
-            Coordinate.From(15, 2),
+            Coordinate.From(1, 2), Coordinate.From(3, 2), Coordinate.From(4, 2), Coordinate.From(5, 2), Coordinate.From(12, 2), Coordinate.From(15, 2),
             Coordinate.From(3, 3), Coordinate.From(6, 3), Coordinate.From(8, 3), Coordinate.From(10, 3), Coordinate.From(13, 3),
-            Coordinate.From(5, 4), Coordinate.From(6, 4), Coordinate.From(7, 4), Coordinate.From(9, 4), Coordinate.From(10, 4),
-            Coordinate.From(12, 4),
-            Coordinate.From(7, 5), Coordinate.From(9, 5), Coordinate.From(10, 5), Coordinate.From(13, 5), Coordinate.From(14, 5),
-            Coordinate.From(15, 5),
+            Coordinate.From(5, 4), Coordinate.From(6, 4), Coordinate.From(7, 4), Coordinate.From(9, 4), Coordinate.From(10, 4), Coordinate.From(12, 4),
+            Coordinate.From(7, 5), Coordinate.From(9, 5), Coordinate.From(10, 5), Coordinate.From(13, 5), Coordinate.From(14, 5), Coordinate.From(15, 5),
             Coordinate.From(2, 6), Coordinate.From(5, 6), Coordinate.From(6, 6), Coordinate.From(12, 6),
-            Coordinate.From(2, 7), Coordinate.From(4, 7), Coordinate.From(5, 7), Coordinate.From(7, 7), Coordinate.From(9, 7), Coordinate.From(10, 7),
-            Coordinate.From(13, 7), Coordinate.From(14, 7), Coordinate.From(15, 7),
+            Coordinate.From(2, 7), Coordinate.From(4, 7), Coordinate.From(5, 7), Coordinate.From(7, 7), Coordinate.From(9, 7), Coordinate.From(10, 7), Coordinate.From(13, 7), Coordinate.From(14, 7), Coordinate.From(15, 7),
         };
         grid.SwitchCoordinatesStatus(coordinates);
         grid.SetDefaultStartCoordinate();
         grid.SetDefaultEndCoordinate();
 
         var tree = grid.GenerateTree();
-        var routes = tree.GetRoutesFromStartToDeadEnds(true).ToList();
+        var routes = tree.OptimizeRoutes();
+
     }
 }
