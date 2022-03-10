b60441caeac069a0eec6159db54a55be5c338ae8
Thomas Vuille <thomas@newtomsoft.fr>
2022-03-10 18:05:48 +0100

    blazor tests

---
+++ LaserBrainTwister.UI.Blazor.Tests/LaserTests.cs
@@ -0,0 +1,47 @@
+using Bunit;
+using LaserBrainTwister.UI.Blazor.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace LaserBrainTwister.UI.Blazor.Tests;
+public class LaserTests
+{
+    [Fact]
+    public void DisableCoordinate()
+    {
+        using var testContext = new TestContext();
+        var jsrMock = new Mock<IJSRuntime>();
+        testContext.Services.AddSingleton(jsrMock.Object);
+        var component = testContext.RenderComponent<Laser>();
+
+        const string btnId = "#btn0_0";
+        var btn = component.Find(btnId);
+        btn.ClassName!.ShouldContain(Laser.BtnDisable);
+        btn.ClassName!.ShouldNotContain(Laser.BtnEnabled);
+
+        btn.Click();
+        btn.ClassName!.ShouldContain(Laser.BtnEnabled);
+        btn.ClassName!.ShouldNotContain(Laser.BtnDisable);
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        using var testContext = new TestContext();
+        var jsrMock = new Mock<IJSRuntime>();
+        testContext.Services.AddSingleton(jsrMock.Object);
+        var component = testContext.RenderComponent<Laser>();
+
+        component.Find("#btn0_0").Click();
+        component.Find("#btn1_0").Click();
+        component.Find("#btn1_1").Click();
+
+        component.Find("#generateTwoWayTree").Click();
+        var canvas = component.Find("#canvas");
+
+        //cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
+}
