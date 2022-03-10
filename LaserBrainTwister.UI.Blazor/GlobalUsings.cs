c6c7f06043e260788b4510b17a7438f9802f55a1
Thomas Vuille <thomas@newtomsoft.fr>
2022-03-10 18:05:39 +0100

    blazor refacror

--- LaserBrainTwister.UI.Blazor/GlobalUsings.cs
+++ LaserBrainTwister.UI.Blazor/GlobalUsings.cs
@@ -1,1 +1,5 @@
-﻿
+﻿global using LaserBrainTwister.Domain;
+global using LaserBrainTwister.Domain.Nodes;
+global using LaserBrainTwister.Domain.Routes;
+global using LaserBrainTwister.Domain.Segments;
+global using Microsoft.JSInterop;
