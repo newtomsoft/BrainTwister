340e677b1377f970d3b28532bb2b8baae474a44c
Thomas Vuille <thomas@newtomsoft.fr>
2022-03-12 10:19:07 +0100

    change compare

--- LaserBrainTwister.Domain/Coordinate.cs
+++ LaserBrainTwister.Domain/Coordinate.cs
@@ -4,7 +4,7 @@ public record Coordinate(Abscissa X, Ordinate Y) : IComparable
 {
     public static Coordinate From(int x, int y) => new((Abscissa)x, (Ordinate)y);
 
-    public int CompareTo(object? obj) => obj is not Coordinate(var x, var y) ? 1 : X != x ? X.CompareTo(x) : Y.CompareTo(y);
+    public int CompareTo(object? obj) => obj is not Coordinate(var x, var y) ? 1 : Y == y ? X.CompareTo(x) : Y.CompareTo(y);
 
     public override string ToString() => $"({X},{Y})";
 }
